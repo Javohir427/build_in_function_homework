4a5cf2b02d92ca3f08ab53a7a175e4f67104f6f6
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-23 20:50:21 +0500

    06

--- build_func06.py
+++ build_func06.py
@@ -1,2 +1,11 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func06
+#from multiprocessing.connection import answer_challenge
+
+
+#from multiprocessing.connection import answer_challenge
+
+
+a = 3.456
+answer=((round(a,2)))
+print(answer)
