b8572f5ae716a5707e3ff23d0b372229ce55eb94
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-23 10:55:20 +0500

    04

--- build_func04.py
+++ build_func04.py
@@ -1,2 +1,8 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func04
+from multiprocessing.connection import answer_challenge
+
+
+n = 4
+answer = pow(((n+2)/3),2)
+print(answer)
